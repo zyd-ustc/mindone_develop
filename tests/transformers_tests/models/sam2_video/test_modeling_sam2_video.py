2beca3bbdf536b94d48c1816eb07a7fdbef5a757
zyd-ustc <131000367+zyd-ustc@users.noreply.github.com>
2025-11-21 14:52:07 +0800

    Update test_modeling_sam2_video.py

--- tests/transformers_tests/models/sam2_video/test_modeling_sam2_video.py
+++ tests/transformers_tests/models/sam2_video/test_modeling_sam2_video.py
@@ -32,7 +32,7 @@ import mindspore as ms
 
 from tests.modeling_test_utils import compute_diffs, generalized_parse_args, get_modules
 from tests.transformers_tests.models.modeling_common import floats_numpy
-from mindone.transformers.models.sam2_video.modeling_sam2_video import msSam2VideoInferenceSession
+from mindone.transformers.models.sam2_video.modeling_sam2_video import Sam2VideoInferenceSession as msSam2VideoInferenceSession
 
 
 DTYPE_AND_THRESHOLDS = {"fp32": 5e-4, "fp16": 5e-3, "bf16": 5e-2}
